44295508aa4d965565c0292f3c60f05695d37ced
francescotroiano98 <francesco-troiano-98@libero.it>
2023-05-17 15:20:00 +0200

    add GAME section (IF)

--- JS/script.js
+++ JS/script.js
@@ -3,3 +3,13 @@ let utente = Math.floor(Math.random() * 6) + 1;
 
 let com = Math.floor(Math.random() * 6) + 1;
     console.log(parseInt(com));
+
+if (utente > com) {
+    alert("Hai Vinto");
+    
+} else if (utente < com) {
+    alert("Hai perso");
+    
+} else if (utente = com) {
+    alert("Hai pareggiato");
+}
