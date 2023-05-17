b3a2d6e000396dc2dcd648f0ef07ae3062f6ca28
francescotroiano98 <francesco-troiano-98@libero.it>
2023-05-17 15:37:02 +0200

    added some layout

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,21 @@
-let utente = Math.floor(Math.random() * 6) + 1;
-  console.log(parseInt(utente));
 
-let com = Math.floor(Math.random() * 6) + 1;
-    console.log(parseInt(com));
+const button = document.querySelector("button.mybutton");
+button.addEventListener("click", function(){
 
-if (utente > com) {
-    alert("Hai Vinto");
-    
-} else if (utente < com) {
-    alert("Hai perso");
-    
-} else if (utente = com) {
-    alert("Hai pareggiato");
-}
+    let utente = Math.floor(Math.random() * 6) + 1;
+    console.log(parseInt(utente));
+    document.getElementById("utente").innerHTML = utente;
+
+    let com = Math.floor(Math.random() * 6) + 1;
+        console.log(parseInt(com));
+        document.getElementById("com").innerHTML = com;
+    if (utente > com) {
+        alert("Hai Vinto");
+
+    } else if (utente < com) {
+        alert("Hai perso");
+        
+    } else if (utente = com) {
+        alert("Hai pareggiato");
+    }
+})
