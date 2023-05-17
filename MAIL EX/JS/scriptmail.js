9f7af0f7f76e343f7eb6173cc910f175ceac7daf
francescotroiano98 <francesco-troiano-98@libero.it>
2023-05-17 16:09:16 +0200

    Done pseudo soluction with logic port OR

--- MAIL EX/JS/scriptmail.js
+++ MAIL EX/JS/scriptmail.js
@@ -1,1 +1,13 @@
-const mail = ["francescototti10@gmail.com", "paulodybala21@gmail.com", "lionelmessi@gamil.com"];
+const mailList = ["francescototti10@gmail.com", "paulodybala21@gmail.com", "lionelmessi@gamil.com"];
+
+const email = prompt("What's your email?");
+
+if (mailList[0 || 1 || 2] === email) {
+    console.log(mailList)
+    console.log(email)
+    alert("Puoi entrare")
+}  else {
+    alert("DOVEVI PRENOTARE L'INGRESSO, NON PUOI ENTRARE");
+    console.log(mailList)
+    console.log(email)
+}
